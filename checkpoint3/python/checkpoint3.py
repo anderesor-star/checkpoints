fa20fbf381e4b45027e5a42138d60f581c7b6905
anderesor-star <anderesor@gmail.com>
2026-03-26 20:02:47 +0100

    Explica qué cambiaste

---
+++ checkpoint3/python/checkpoint3.py
@@ -0,0 +1,36 @@
+#exercise 1
+example_string = 'hello world'
+example_number = 54
+example_list = [1, 2, 3, 4]
+example_boolean = True
+
+#exercise 2
+index_string = example_string[0:3]
+
+#exercise 3
+index_list = example_list[0]
+
+#exercise 4
+new_number = example_number + 10
+
+#exercise 5
+list_index = example_list[-1]
+
+#exercise 6
+names = 'harry,alex,susie,jared,gail,conner'
+example_split = names.split(',')
+
+#exercise 7
+first_word = example_string[0:5].upper()
+new_string = first_word + example_string[5:]
+
+#exercise 8
+example_interpolation = 'Mañana cumpliré ' + str(example_number) + 'años'
+
+#exercise 9
+print("hello world")
+
+#extra
+cadena = 'Y cuando me dijo hola se cortó la llamada'
+print(cadena.index('hola'))
+print(cadena.replace("hola", "adiós"))
