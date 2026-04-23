7ccc96be00a57fd3e82f302be783c4dd93d000ee
anderesor-star <anderesor@gmail.com>
2026-04-23 18:08:47 +0200

    commit

--- checkpoint5/fizzbuzz/fizzbuzz.py
+++ checkpoint5/fizzbuzz/fizzbuzz.py
@@ -4,10 +4,13 @@ def fizzbuzz (numbers):
     for number in range(1, numbers +1):
         if number % 3 == 0 and number % 5 == 0:
             print('Fizzbuzz')
+            continue
         elif number % 5 == 0:
             print('Buzz')
+            continue
         elif number % 3 == 0:
             print('Fizz')
+            continue
         else:
             print(number)
 
